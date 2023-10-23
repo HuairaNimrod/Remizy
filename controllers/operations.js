7d34ff79f05b291b09ede8672bbca73cc3bdbdea
Nimrod Huaira <nimax29@gmail.com>
2023-10-23 13:47:23 -0600

    put

--- controllers/operations.js
+++ controllers/operations.js
@@ -52,7 +52,7 @@ const createOperation = (req, res) => {
     // const operation = new Operations(req.body);
 
     try {
-      await Operations.findByIdAndUpdate(req.params.id, req.body);
+      await Operations.findByIdAndUpdate(req.params.id, !req.body.sender || !req.body.receiver || !req.body.amount || !req.body.rate|| !req.body.depositId|| !req.body.status|| !req.body.comments);
       res.status(200).send('Operation update');
     } catch (error) {
       res.status(500).send(error);
