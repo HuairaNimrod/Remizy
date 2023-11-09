e07a379a504caaac5aee882769891045c55c9035
Nimrod Huaira <nimax29@gmail.com>
2023-11-09 00:52:29 -0700

    finished

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+const path = require('path');
+const { auth } = require('express-openid-connect');
+const router = require('./routes/index');
+
+
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ;
 
-app
-    .use(bodyParser.json())
-    .use((req, res, next) =>{
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+
+
+
+app.use(bodyParser.json());
+app.use((req, res, next) =>{
         res.setHeader('Access-Control-Allow-Origin','*') // instructs browsers to permit that origin to access server resources for requests without credentials
         next();
-    })
-    .use('/', require('./routes')); //accessing routes folder
+    });
 
-    
+
+const config = {
+      authRequired: false,
+      auth0Logout: true
+    }; 
+  
+app.use(auth(config));
+
+
+    // Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+      res.locals.user = req.oidc.user;
+      next();
+    });
+    // .use('/', require('./routes')); //accessing routes folder
+
+app.use('/', router);
+
 const db = require('./models');
 db.mongoose
   .connect(db.url, {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const routes =  express.Router();
+// const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
-routes.use('/', require('./swagger'));
-routes.use('/operation', require('./operations'))
+const dotenv = require('dotenv');
+dotenv.config();
+
+routes.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+
+    if (req.oidc.isAuthenticated()){
+      
+      routes.use('/', require('./swagger'));
+      routes.use('/operation', require('./operations'));
+    }
+  }
+);
+
+routes.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
+
 
 module.exports = routes;
